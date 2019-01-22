23ca2d98023c567869b20125a28634292ac6158a
Kevin <kevin@cestfrais.fr>
2019-01-22 09:53:01 +0100

    Design update (Background & pons) + JS loggin for solution study

--- js/src/script.js
+++ js/src/script.js
@@ -1,17 +1,18 @@
-
 // Game Settings
 var boardSize = "8x8";
 
 var board = jsboard.board({attach: "game", size: boardSize});
 var white = jsboard.piece({
-  text: "",
+  text: "W",
+  color: "transparent",
   background: "url('./img/white-pon.png') no-repeat",
   width: "90px",
   height: "90px",
   margin: "0 auto"
 });
 var black = jsboard.piece({
-  text: "",
+  text: "B",
+  color: "transparent",
   background: "url('./img/black-pon.png') no-repeat",
   width: "90px",
   height: "90px",
@@ -19,26 +20,54 @@ var black = jsboard.piece({
 });
 
 // Styling
-board.cell("each").style({width: "100px", height: "100px", background:"green"});
+board.cell("each").style({
+  width: "100px",
+  height: "100px",
+  background: "green",
+  backgroundImage: "url('./img/black-paper.png')",
+  borderRadius: "3px"
+});
 
-// Board behavoir
+// Game initialization
+gameInit();
+
+// Game click handler
 var turn = true;
 board.cell("each").on("click", function() {
   if (board.cell(this).get() === null) {
     if (turn) {
-      // IF nearby cells are not empty AND not white 
-      var location = board.cell(this).where();
-      console.log("pièce blanche : " + location);
-      console.log(board.matrix);
+      // Call function to check the rule
+      var matrix = board.matrix(); // full board
+      var currentCell = board.cell(this).where(); // place where the player wants to play
+      console.log(matrix);
+      console.log(currentCell);
       board.cell(this).place(white.clone());
     } else {
-      // IF nearby cells are not empty AND not black
-      var location = board.cell(this).where();
-      console.log("pièce noir : " + location);
-      console.log(board.matrix);
-      
+      // Call function to check the rule
+      var matrix = board.matrix();
+      var currentCell = board.cell(this).where();
+      console.log(matrix);
+      console.log(currentCell);
       board.cell(this).place(black.clone());
     }
     turn = !turn;
   }
 });
+
+// +1 et -1 dans les directions N/S/E/O dans une double boucle i and j.
+// Function is check if the player can put the pon where is choose to
+// if not, cell will be denied : UI/UX to be defined (first will be red)
+// if ok, play a little chim sound
+function checkAllPossibilities(position, matrix) {
+  // var matrix = board.matrix();
+  // var currentCell = board.cell(this).where();
+  // console.log(matrix);
+  // console.log(currentCell);
+}
+
+function gameInit() {
+  board.cell([3, 3]).place(white.clone());
+  board.cell([3, 4]).place(black.clone());
+  board.cell([4, 3]).place(black.clone());
+  board.cell([4, 4]).place(white.clone());
+}
